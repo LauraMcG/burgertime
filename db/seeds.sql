da9736f5c190048cc5773978a0af451e50e7d644
Laura McGinn <mcginn.laura@gmail.com>
2017-04-30 10:28:18 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES 
+('Double Bacon Artery Stopper', false),
+('Mega Beffcake', true),
+('Barbecue Chicken Ranch', false);
---
+++ orm.js
@@ -0,0 +1,5 @@
+// var connection = require("../config/connection.js");
+
+// var orm = console.log('orm connected!');
+
+// module.exports = orm;
