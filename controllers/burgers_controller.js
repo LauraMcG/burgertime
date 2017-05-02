972e9875be76ab2224c47d2911037c24c2984caf
Laura McGinn <mcginn.laura@gmail.com>
2017-05-02 12:39:42 -0500

    basic handlebars structure -- still needs tweaking

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,7 @@ var express = require('express');
 var burger = require('./models/burger.js');
 
 //Create the router for the app, and export the router at the end of your file.
+
+app.get('/', function(req, res) {
+	res.render{'index'};
+}
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-var express = require('express');
-var methodOverride = require('method-override');
+//DEPENDENCIES
 var bodyParser = require('body-parser');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
 
 var orm = require("./config/orm.js");
+
+//setting up express server
+var app = express();
+var port = 3000;
+
+//search for static content in public and serve it.
+app.use(express.static(process.cwd() + '/public'));
+
+// setting up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//utilizing method ovveride to allow us to PUT and DELETE
+app.use(methodOverride("_method"));
+
+//settung up handlebars usage
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
